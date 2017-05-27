efc28abebb49726e2d15222b81946e916bcc836b
tandpfun <dev.tandpfun@gmail.com>
2017-05-27 11:34:12 -0700

    added file~
    yay

---
+++ color.py
@@ -0,0 +1,35 @@
+
+
+
+def welcome():
+	"""Welcome message for Color Codes!"""
+	print "\033[92m* Welcome to: \033[94mColor Codes! *"
+	print "\033[92m* More coming soon!\033[0m *"
+
+def setred():
+	"""Sets your text color to Red"""
+	print "\033[91mText Color Now Red"
+
+def setgreen():
+	"""Sets your text color to Green"""
+	print "\033[92mText Color Now Green"
+
+def setblue():
+	"""Sets your text color to Blue"""
+	print "\033[94mText Color Now Blue"
+
+def setyellow():
+	"""Sets your text color to Yellow"""
+	print "\033[93mText Color Now Yellow"
+def typebold():
+	"""Sets your text Type to Bold"""
+	print "\033[1mText Type Now Bold"
+def setpurple():
+	"""Sets your text Color to purple"""
+	print "\033[95mText Color now purple"
+def typeunderline():
+	"""Sets your text type to Underline"""
+	print "\033[4mText type now Underline"
+def clear():
+	"""Clears all text modifiers"""
+	print "\033[0mText types and colors now cleared"
