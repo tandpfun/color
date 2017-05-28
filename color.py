8a10b166d8223b5444e37cd0caa0d870d6868e60
tandpfun <dev.tandpfun@gmail.com>
2017-05-27 18:39:27 -0700

    Update color.py

--- color.py
+++ color.py
@@ -34,5 +34,5 @@ def clear():
 	"""Clears all text modifiers"""
 	print "\033[0mText types and colors now cleared"
 def end():
-	"""Ends Your Session, Goodbye. You will not loose your color codes"
+	"""Ends Your Session, Goodbye. You will not loose your color codes"""
 	print "Press Ctrl - D. You will not loose your color codes!"
