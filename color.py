ee50feab811b5d9be8003e08343e2adecdfc1167
Codingpro <dev.tandpfun@gmail.com>
2018-01-03 13:06:15 -0800

    New print interface! Useful with CIrcuit Python!

--- color.py
+++ color.py
@@ -1,99 +1,95 @@
-
-
-
 def welcome():
 	"""Welcome message for Color Codes!"""
-	print "\033[92m* Welcome to: \033[94mColor Codes! *"
-	print "\033[92m* More coming soon!\033[0m *"
-
+	print ('\033[92m* Welcome to: \033[94mColor Codes! ')
+	print ('\033[92m* More coming soon!\033[0m *')
 def setred():
-	"""Sets your text color to Red"""
-	print "\033[91mText Color Now Red"
+    """Sets your text color to Red"""
+    print ('\033[91mText Color Now Red"')
 
 def setgreen():
-	"""Sets your text color to Green"""
-	print "\033[92mText Color Now Green"
+    """Sets your text color to Green"""
+    print ('\033[92mText Color Now Green')
 
 def setblue():
-	"""Sets your text color to Blue"""
-	print "\033[94mText Color Now Blue"
+    """Sets your text color to Blue"""
+    print ('\033[94mText Color Now Blue')
 
 def setyellow():
-	"""Sets your text color to Yellow"""
-	print "\033[93mText Color Now Yellow"
+    """Sets your text color to Yellow"""
+    print ('\033[93mText Color Now Yellow')
 def typebold():
-	"""Sets your text Type to Bold"""
-	print "\033[1mText Type Now Bold"
+    """Sets your text Type to Bold"""
+    print ('\033[1mText Type Now Bold')
 def setpurple():
-	"""Sets your text Color to purple"""
-	print "\033[95mText Color now purple"
+    """Sets your text Color to purple"""
+    print ('\033[95mText Color now purple')
 def typeunderline():
-	"""Sets your text type to Underline"""
-	print "\033[4mText type now Underline"
+    """Sets your text type to Underline"""
+    print ('\033[4mText type now Underline')
 def clear():
-	"""Clears all text modifiers"""
-	print "\033[0mText types and colors now cleared"
+    """Clears all text modifiers"""
+    print ('\033[0mText types and colors now cleared')
 def end():
-	"""Ends Your Session, Goodbye. You will not loose your color codes"""
-	print "Press Ctrl - D. You will not loose your color codes!"
+    """Ends Your Session, Goodbye. You will not loose your color codes"""
+    print ('Press Ctrl - D. You will not loose your color codes!')
 def typeblink():
-	"""Blinking text? Whoah!"""
-	print "\033[5mText type set to Blink"
+    """Blinking text? Whoah!"""
+    print ('\033[5mText type set to Blink')
 def typedim():
-	"""Dim Text, Very dim."""
-	print "\033[2mText type set to Dim"
+    """Dim Text, Very dim."""
+    print ('\033[2mText type set to Dim')
 def typeinvert():
-	"""invert my text, that would be nice."""
-	print "\033[7mText type set to Invert"
+    """invert my text, that would be nice."""
+    print ('\033[7mText type set to Invert')
 def setcyan():
-	"""Yay! My favorate color!"""
-	print "\033[36mText color set to Cyan"
+    """Yay! My favorate color!"""
+    print ('\033[36mText color set to Cyan')
 def setlightgray():
-	"""Yay! My favorate color!"""
-	print "\033[37mText color set to Light Gray"
+    """Yay! My favorate color!"""
+    print ('\033[37mText color set to Light Gray')
 def setgray():
-	"""Yay! My favorate color!"""
-	print "\033[90mText color set to gray"
+    """Yay! My favorate color!"""
+    print ('\033[90mText color set to gray')
 def bred():
-	"""Set your background color to red"""
-	print "\033[41mText background set to Red"
+    """Set your background color to red"""
+    print ('\033[41mText background set to Red')
 def bgreen():
-	"""Set your background color to Green"""
-	print "\033[42mText background set to Green"
+    """Set your background color to Green"""
+    print ('\033[42mText background set to Green')
 def byellow():
-	"""Set your background color to Yellow"""
-	print "\033[43mText background set to Yellow"
+    """Set your background color to Yellow"""
+    print ('\033[43mText background set to Yellow')
 def bblue():
-	"""Set your background color to Blue"""
-	print "\033[44mText background set to Blue"
+    """Set your background color to Blue"""
+    print ('\033[44mText background set to Blue')
 def bmagenta():
-	"""Set your background color to Magenta"""
-	print "\033[45mText background set to Magenta"
+    """Set your background color to Magenta"""
+    print ('\033[45mText background set to Magenta')
 def bcyan():
-	"""Set your background color to Cyan"""
-	print "\033[46mText background set to Cyan"
+    """Set your background color to Cyan"""
+    print ('\033[46mText background set to Cyan')
 def blgray():
-	"""Set your background color to Light Grey"""
-	print "\033[47mText background set to Light Grey"
+    """Set your background color to Light Grey"""
+    print ('\033[47mText background set to Light Grey')
 def bgray():
-	"""Set your background color to Gray"""
-	print "\033[100mText background set to Gray"
+    """Set your background color to Gray"""
+    print ('\033[100mText background set to Gray')
 def blred():
-	"""Set your background color to Light red"""
-	print "\033[101mText background set to Light Red"
+    """Set your background color to Light red"""
+    print ('\033[101mText background set to Light Red')
 def blgreen():
-	"""Set your background color to light green"""
-	print "\033[102mText background set to light green"
+    """Set your background color to light green"""
+    print ('\033[102mText background set to light green')
 def blyellow():
-	"""Set your background color to light yellow"""
-	print "\033[103mText background set to light yellow"
+    """Set your background color to light yellow"""
+    print ('\033[103mText background set to light yellow')
 def blblue():
-	"""Set your background color to light Blue"""
-	print "\033[104mText background set to light Blue"
+    """Set your background color to light Blue"""
+    print ('\033[104mText background set to light Blue')
 def blmagenta():
-	"""Set your background color to light magenta"""
-	print "\033[105mText background set to light magenta"
+    """Set your background color to light magenta"""
+    print ('\033[105mText background set to light magenta')
 def blcyan():
-	"""Set your background color to light cyan"""
-	print "\033[106mText background set to light cyan"
+    """Set your background color to light cyan"""
+    print ('\033[106mText background set to light cyan')
 
